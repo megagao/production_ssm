ccd8a1fef0236f7ea03b065d848c1a2f1bb0d29f
megagao <megagao@126.com>
2016-09-25 15:35:22 +0800

    实现了shrio + ajax 的登录方式

--- src/main/java/org/hqu/production_ms/controller/LoginController.java
+++ src/main/java/org/hqu/production_ms/controller/LoginController.java
@@ -10,8 +10,16 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
 import org.hqu.production_ms.domain.ActiveUser;
+import org.hqu.production_ms.exception.CustomException;
 import org.hqu.production_ms.service.SysService;
 
  /**
@@ -63,6 +71,34 @@ public class LoginController {
 			map.put("msg", "用户名或密码错误！");
 		}
 		return map; 
+	}
+	
+	/**
+	 * ajax登录 spring mvc 实现
+	 */
+	@RequestMapping(value = "/ajaxLogin")
+	public @ResponseBody Map<String,Object> ajaxLogin(@RequestParam String username, @RequestParam String password){
+	    
+		Subject currentUser = SecurityUtils.getSubject();
+	    
+	    Map<String,Object> map = new HashMap<String,Object>();  
+	    if (!currentUser.isAuthenticated()) {
+	    	
+	    	UsernamePasswordToken token = new UsernamePasswordToken(username, password);
+	         
+	        try{
+	            currentUser.login(token);
+	        }catch(UnknownAccountException ex){
+	        	map.put("msg", "account_error");
+	        }catch(IncorrectCredentialsException ex){
+	        	map.put("msg", "password_error");
+	        }catch(AuthenticationException ex){
+	        	map.put("msg", "authentication_error");
+	        }
+	    }
+	     
+	    //返回json数据
+	  return map;
 	}
 	
 	
