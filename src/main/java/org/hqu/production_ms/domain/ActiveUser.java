e2c10334e5b350f2adf0975f2d5504687149fdae
megagao <megagao@126.com>
2016-09-26 21:27:28 +0800

    添加了封装的用户信息，认证成功后将此信息记录到session

---
+++ src/main/java/org/hqu/production_ms/domain/ActiveUser.java
@@ -0,0 +1,65 @@
+package org.hqu.production_ms.domain;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.hqu.production_ms.domain.authority.SysPermission;
+
+
+/**
+ * 用户身份信息，存入session 由于tomcat将session会序列化在本地硬盘上，所以使用Serializable接口
+ * 
+ * @author Thinkpad
+ * 
+ */
+public class ActiveUser implements java.io.Serializable {
+	private String userid;//用户id（主键）
+	private String usercode;// 用户账号
+	private String username;// 用户名称
+
+	private List<SysPermission> menus;// 菜单
+	private List<SysPermission> permissions;// 权限
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+
+	public String getUsercode() {
+		return usercode;
+	}
+
+	public void setUsercode(String usercode) {
+		this.usercode = usercode;
+	}
+
+	public String getUserid() {
+		return userid;
+	}
+
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+
+	public List<SysPermission> getMenus() {
+		return menus;
+	}
+
+	public void setMenus(List<SysPermission> menus) {
+		this.menus = menus;
+	}
+
+	public List<SysPermission> getPermissions() {
+		return permissions;
+	}
+
+	public void setPermissions(List<SysPermission> permissions) {
+		this.permissions = permissions;
+	}
+
+	
+}
