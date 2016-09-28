181e660910540403e7d9a6834b57ceafbf5250f8
megagao <megagao@126.com>
2016-09-28 13:07:14 +0800

    添加了图片上传的service

---
+++ src/main/java/org/hqu/production_ms/service/PictureService.java
@@ -0,0 +1,10 @@
+package org.hqu.production_ms.service;
+
+import java.util.Map;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface PictureService {
+	
+	Map uploadPicture(MultipartFile uploadFile);
+}
