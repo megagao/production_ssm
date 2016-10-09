25e9a276a911a5e3644d19d080bf2c9bf7abae9a
megagao <megagao@126.com>
2016-10-09 13:42:55 +0800

    添加了两个更新方法的注释

--- src/main/java/org/hqu/production_ms/service/OrderService.java
+++ src/main/java/org/hqu/production_ms/service/OrderService.java
@@ -17,8 +17,10 @@ public interface OrderService {
 
 	CustomResult insert(COrderPO cOrder);
 
+	//更新部分字段，用的是updateSelective判断非空的字段进行更新
     CustomResult update(COrderPO cOrder);
     
+    //更新全部字段，不判断非空，直接进行更新
     CustomResult updateAll(COrderPO cOrder);
 
     CustomResult changeStatus(String[] ids);
