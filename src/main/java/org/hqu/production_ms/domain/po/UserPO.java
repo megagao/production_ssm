99d476ed6e310b5fac933728e49248a47cfcb3e0
megagao <megagao@126.com>
2016-10-21 16:21:57 +0800

    添加了用户信息显示的包装对象

---
+++ src/main/java/org/hqu/production_ms/domain/po/UserPO.java
@@ -0,0 +1,53 @@
+package org.hqu.production_ms.domain.po;
+
+public class UserPO {
+	private String id;
+
+    private String username;
+
+    private String password;
+    
+    private String locked;
+    
+    private String role;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getLocked() {
+		return locked;
+	}
+
+	public void setLocked(String locked) {
+		this.locked = locked;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+}
