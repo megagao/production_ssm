c42a41cfe585db67963baa5a8d11d2cf182256e0
megagao <megagao@126.com>
2016-10-22 16:05:03 +0800

    添加了文件

---
+++ src/main/java/org/hqu/production_ms/service/PermissionService.java
@@ -0,0 +1,26 @@
+package org.hqu.production_ms.service;
+
+import java.util.List;
+
+import org.hqu.production_ms.domain.CustomResult;
+import org.hqu.production_ms.domain.EUDataGridResult;
+import org.hqu.production_ms.domain.authority.SysRolePermission;
+
+public interface PermissionService {
+	
+	List<SysRolePermission> find();  
+	
+	EUDataGridResult getList(int page, int rows, SysRolePermission sysRolePermission);
+	
+	SysRolePermission get(String string);
+	
+	CustomResult delete(String string);
+
+	CustomResult insert(SysRolePermission sysRolePermission);
+
+	//更新部分字段，用的是updateSelective判断非空的字段进行更新
+    CustomResult update(SysRolePermission sysRolePermission);
+    
+    //更新全部字段，不判断非空，直接进行更新
+    CustomResult updateAll(SysRolePermission sysRolePermission);
+}
