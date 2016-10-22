1ce2301033e573830e76a1076694a80e8d8e0b7b
megagao <megagao@126.com>
2016-10-22 16:05:14 +0800

    添加了文件

---
+++ src/main/java/org/hqu/production_ms/service/impl/PermissionServiceImpl.java
@@ -0,0 +1,90 @@
+package org.hqu.production_ms.service.impl;
+
+import java.util.List;
+
+import org.hqu.production_ms.domain.CustomResult;
+import org.hqu.production_ms.domain.EUDataGridResult;
+import org.hqu.production_ms.domain.authority.SysRolePermission;
+import org.hqu.production_ms.domain.authority.SysRolePermissionExample;
+import org.hqu.production_ms.mapper.authority.SysRolePermissionMapper;
+import org.hqu.production_ms.service.PermissionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+
+@Service
+public class PermissionServiceImpl implements PermissionService{
+
+	@Autowired
+	SysRolePermissionMapper sysRolePermissionMapper;
+	
+	@Override
+	public EUDataGridResult getList(int page, int rows, SysRolePermission sysRolePermission) {
+		//查询列表
+		SysRolePermissionExample example = new SysRolePermissionExample();
+		//分页处理
+		PageHelper.startPage(page, rows);
+		List<SysRolePermission> list = sysRolePermissionMapper.selectByExample(example);
+		//创建一个返回值对象
+		EUDataGridResult result = new EUDataGridResult();
+		result.setRows(list);
+		//取记录总条数
+		PageInfo<SysRolePermission> pageInfo = new PageInfo<>(list);
+		result.setTotal(pageInfo.getTotal());
+		return result;
+	}
+
+	@Override
+	public SysRolePermission get(String string) {
+		
+		return sysRolePermissionMapper.selectByPrimaryKey(string);
+	}
+
+	@Override
+	public CustomResult delete(String string) {
+		int i = sysRolePermissionMapper.deleteByPrimaryKey(string);
+		if(i>=0){
+			return CustomResult.ok();
+		}else{
+			return null;
+		}
+	}
+
+	@Override
+	public CustomResult insert(SysRolePermission sysRolePermission) {
+		int i = sysRolePermissionMapper.insert(sysRolePermission);
+		if(i>=0){
+			return CustomResult.ok();
+		}else{
+			return null;
+		}
+	}
+
+	@Override
+	public CustomResult update(SysRolePermission sysRolePermission) {
+		int i = sysRolePermissionMapper.updateByPrimaryKeySelective(sysRolePermission);
+		if(i>=0){
+			return CustomResult.ok();
+		}else{
+			return null;
+		}
+	}
+
+	@Override
+	public CustomResult updateAll(SysRolePermission sysRolePermission) {
+		int i = sysRolePermissionMapper.updateByPrimaryKey(sysRolePermission);
+		if(i>=0){
+			return CustomResult.ok();
+		}else{
+			return null;
+		}
+	}
+
+	@Override
+	public List<SysRolePermission> find() {
+		SysRolePermissionExample example = new SysRolePermissionExample();
+		return sysRolePermissionMapper.selectByExample(example);
+	}
+}
