2410af5a8c3bb46e7abe23d0ee56de19addf503b
megagao <megagao@126.com>
2016-10-23 14:26:47 +0800

    修改了权限的请求参数

--- src/main/java/org/hqu/production_ms/controller/PermissionController.java
+++ src/main/java/org/hqu/production_ms/controller/PermissionController.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -36,6 +37,12 @@ public class PermissionController {
 	@ResponseBody
 	public List<SysRolePermission> getData() {
 		return permissionService.find();
+	}
+	
+	@RequestMapping("/get_permission")
+	@ResponseBody
+	public SysRolePermission getPermission(String roleId) {
+		return permissionService.getByRoleId(roleId);
 	}
 	
 	@RequestMapping("/add")
