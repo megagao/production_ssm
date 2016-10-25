f97065d932b78add401f3c39e91dcb52d2844f58
megagao <megagao@126.com>
2016-10-25 14:51:43 +0800

    删除了无用的引用

--- src/main/java/org/hqu/production_ms/controller/DeviceController.java
+++ src/main/java/org/hqu/production_ms/controller/DeviceController.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
