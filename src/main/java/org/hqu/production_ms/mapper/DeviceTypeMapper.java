61ffd3bc3f5d26bc5ec9ed490f969f3739fe88e0
megagao <megagao@126.com>
2016-10-26 22:18:48 +0800

    添加了数据库更改后逆向工程生成的设备相关文件

---
+++ src/main/java/org/hqu/production_ms/mapper/DeviceTypeMapper.java
@@ -0,0 +1,30 @@
+package org.hqu.production_ms.mapper;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+import org.hqu.production_ms.domain.DeviceType;
+import org.hqu.production_ms.domain.DeviceTypeExample;
+
+public interface DeviceTypeMapper {
+    int countByExample(DeviceTypeExample example);
+
+    int deleteByExample(DeviceTypeExample example);
+
+    int deleteByPrimaryKey(String deviceTypeId);
+
+    int insert(DeviceType record);
+
+    int insertSelective(DeviceType record);
+
+    List<DeviceType> selectByExample(DeviceTypeExample example);
+
+    DeviceType selectByPrimaryKey(String deviceTypeId);
+
+    int updateByExampleSelective(@Param("record") DeviceType record, @Param("example") DeviceTypeExample example);
+
+    int updateByExample(@Param("record") DeviceType record, @Param("example") DeviceTypeExample example);
+
+    int updateByPrimaryKeySelective(DeviceType record);
+
+    int updateByPrimaryKey(DeviceType record);
+}
