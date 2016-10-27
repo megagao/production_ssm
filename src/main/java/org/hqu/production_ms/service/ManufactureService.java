0d6a4b8dbf6e721e711cd7fc55f9734a1e285d0a
megagao <megagao@126.com>
2016-10-27 21:54:48 +0800

    添加了生产计划service接口

---
+++ src/main/java/org/hqu/production_ms/service/ManufactureService.java
@@ -0,0 +1,26 @@
+package org.hqu.production_ms.service;
+
+import org.hqu.production_ms.domain.CustomResult;
+import org.hqu.production_ms.domain.EUDataGridResult;
+import org.hqu.production_ms.domain.Manufacture;
+import org.hqu.production_ms.domain.po.ManufacturePO;
+
+public interface ManufactureService {
+	
+	EUDataGridResult getList(int page, int rows);
+	
+	Manufacture get(String string);
+	
+	CustomResult delete(String string);
+
+	CustomResult deleteBatch(String[] ids);
+
+	CustomResult insert(ManufacturePO manufacture);
+
+	//更新部分字段，用的是updateSelective判断非空的字段进行更新
+    CustomResult update(ManufacturePO manufacture);
+    
+    //更新全部字段，不判断非空，直接进行更新
+    CustomResult updateAll(ManufacturePO manufacture);
+    
+}
