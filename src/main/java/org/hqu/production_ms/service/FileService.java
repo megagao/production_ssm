8f5acd0b40bc7b1cc57f017b63430b0afb23be49
megagao <megagao@126.com>
2016-10-27 21:55:19 +0800

    为map指定了泛型

--- src/main/java/org/hqu/production_ms/service/FileService.java
+++ src/main/java/org/hqu/production_ms/service/FileService.java
@@ -6,7 +6,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 public interface FileService {
 	
-	Map uploadFile(MultipartFile uploadFile);
+	Map<String,Object> uploadFile(MultipartFile uploadFile);
 	
 	boolean deleteFile(String fileName);
 }
