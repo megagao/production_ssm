bdac760922476bd26f2a72fbedaf7b7c6939fd73
megagao <megagao@126.com>
2016-10-27 21:56:21 +0800

    添加了订单查找方法

--- src/main/java/org/hqu/production_ms/service/OrderService.java
+++ src/main/java/org/hqu/production_ms/service/OrderService.java
@@ -1,11 +1,15 @@
 package org.hqu.production_ms.service;
 
+import java.util.List;
+
 import org.hqu.production_ms.domain.COrder;
 import org.hqu.production_ms.domain.CustomResult;
 import org.hqu.production_ms.domain.EUDataGridResult;
 import org.hqu.production_ms.domain.po.COrderPO;
 
 public interface OrderService {
+	
+	List<COrder> find(); 
 	
 	EUDataGridResult getList(int page, int rows, COrder cOrder);
 	
