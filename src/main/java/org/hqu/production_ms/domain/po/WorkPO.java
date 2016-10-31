1eb9790e4b14a3d8e2c59901aed0edb33ad3a3c1
megagao <megagao@126.com>
2016-10-31 19:35:46 +0800

    添加生产派工的前台数据封装对象

---
+++ src/main/java/org/hqu/production_ms/domain/po/WorkPO.java
@@ -0,0 +1,64 @@
+package org.hqu.production_ms.domain.po;
+
+
+public class WorkPO {
+	private String workId;
+
+    private String processNumber;
+
+    private String productId;
+
+    private String processId;
+
+    private String equipmentId;
+
+    private Integer rating;
+
+    public String getWorkId() {
+        return workId;
+    }
+
+    public void setWorkId(String workId) {
+        this.workId = workId == null ? null : workId.trim();
+    }
+
+    public String getProcessNumber() {
+        return processNumber;
+    }
+
+    public void setProcessNumber(String processNumber) {
+        this.processNumber = processNumber == null ? null : processNumber.trim();
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId == null ? null : productId.trim();
+    }
+
+    public String getProcessId() {
+        return processId;
+    }
+
+    public void setProcessId(String processId) {
+        this.processId = processId == null ? null : processId.trim();
+    }
+
+    public String getEquipmentId() {
+        return equipmentId;
+    }
+
+    public void setEquipmentId(String equipmentId) {
+        this.equipmentId = equipmentId == null ? null : equipmentId.trim();
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+}
