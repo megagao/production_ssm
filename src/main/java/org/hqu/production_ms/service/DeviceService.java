4c00148a83756eb31a53b13ae8461a50c7c11ba0
megagao <megagao@126.com>
2016-11-01 20:33:52 +0800

    添加了设备相关表的service接口

--- src/main/java/org/hqu/production_ms/service/DeviceService.java
+++ src/main/java/org/hqu/production_ms/service/DeviceService.java
@@ -1,28 +1,31 @@
 package org.hqu.production_ms.service;
 
-import org.hqu.production_ms.domain.CustomResult;
 import org.hqu.production_ms.domain.Device;
-import org.hqu.production_ms.domain.EUDataGridResult;
+import org.hqu.production_ms.domain.custom.CustomResult;
+import org.hqu.production_ms.domain.custom.EUDataGridResult;
 
 public interface DeviceService {
+
 	
+	//List
 	EUDataGridResult getList(int page, int rows, Device device);
-	
 	Device get(String string);
-	
-	CustomResult delete(String string);
 
-	CustomResult deleteBatch(String[] ids);
+	//List_Name
+	EUDataGridResult getList_Name();
+	//List_Type
+	EUDataGridResult getList_Type();
 
+	//Insert
 	CustomResult insert(Device device);
+	CustomResult insertBatch(Device[] devices);
+	
+	//Delet
+	CustomResult delete(String deviceId);
+	CustomResult deleteBatch(String[] deviceIds);
 
-	//更新部分字段，用的是updateSelective判断非空的字段进行更新
+	//Update
     CustomResult update(Device device);
+    CustomResult updateBatch(Device[] devices);
     
-    //更新全部字段，不判断非空，直接进行更新
-    CustomResult updateAll(Device device);
-    
-    CustomResult updateNote(Device device);
-
-    CustomResult changeStatus(String[] ids);
 }
