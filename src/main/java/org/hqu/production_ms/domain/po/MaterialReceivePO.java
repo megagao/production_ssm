a3ace4a586379250a0ab60d742bafc45d3a3196a
megagao <megagao@126.com>
2016-11-26 14:37:45 +0800

    添加了物料收入PO对象

---
+++ src/main/java/org/hqu/production_ms/domain/po/MaterialReceivePO.java
@@ -0,0 +1,73 @@
+package org.hqu.production_ms.domain.po;
+import java.util.Date;
+public class MaterialReceivePO {
+    private String receiveId;
+
+    private String materialId;
+
+    private Integer ammount;
+
+    private Date receiveDate;
+
+    private String sender;
+
+    private String receiver;
+
+    private String note;
+
+    public String getReceiveId() {
+        return receiveId;
+    }
+
+    public void setReceiveId(String receiveId) {
+        this.receiveId = receiveId == null ? null : receiveId.trim();
+    }
+
+    public String getMaterialId() {
+        return materialId;
+    }
+
+    public void setMaterialId(String materialId) {
+        this.materialId = materialId == null ? null : materialId.trim();
+    }
+
+    public Integer getAmmount() {
+        return ammount;
+    }
+
+    public void setAmmount(Integer ammount) {
+        this.ammount = ammount;
+    }
+
+    public Date getReceiveDate() {
+        return receiveDate;
+    }
+
+    public void setReceiveDate(Date receiveDate) {
+        this.receiveDate = receiveDate;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender == null ? null : sender.trim();
+    }
+
+    public String getreceiver() {
+        return receiver;
+    }
+
+    public void setreceiver(String receiver) {
+        this.receiver = receiver == null ? null : receiver.trim();
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note == null ? null : note.trim();
+    }
+}
