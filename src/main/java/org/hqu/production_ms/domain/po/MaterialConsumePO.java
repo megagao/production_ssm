367f6d44727b9e7c3592d0fb02b80e0616746c67
megagao <megagao@126.com>
2016-11-26 14:37:33 +0800

    添加了物料消耗PO对象

---
+++ src/main/java/org/hqu/production_ms/domain/po/MaterialConsumePO.java
@@ -0,0 +1,85 @@
+package org.hqu.production_ms.domain.po;
+
+import java.util.Date;
+
+public class MaterialConsumePO {
+    private String consumeId;
+
+    private String workId;
+
+    private String materialId;
+
+    private Integer consumeAmount;
+
+    private Date consumeDate;
+
+    private String sender;
+
+    private String receiver;
+
+    private String note;
+
+    public String getConsumeId() {
+        return consumeId;
+    }
+
+    public void setConsumeId(String consumeId) {
+        this.consumeId = consumeId == null ? null : consumeId.trim();
+    }
+
+    public String getWorkId() {
+        return workId;
+    }
+
+    public void setWorkId(String workId) {
+        this.workId = workId == null ? null : workId.trim();
+    }
+
+    public String getMaterialId() {
+        return materialId;
+    }
+
+    public void setMaterialId(String materialId) {
+        this.materialId = materialId == null ? null : materialId.trim();
+    }
+
+    public Integer getConsumeAmount() {
+        return consumeAmount;
+    }
+
+    public void setConsumeAmount(Integer consumeAmount) {
+        this.consumeAmount = consumeAmount;
+    }
+
+    public Date getConsumeDate() {
+        return consumeDate;
+    }
+
+    public void setConsumeDate(Date consumeDate) {
+        this.consumeDate = consumeDate;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender == null ? null : sender.trim();
+    }
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(String receiver) {
+        this.receiver = receiver == null ? null : receiver.trim();
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note == null ? null : note.trim();
+    }
+}
