c7a9a9790146522c156afb5257fe258778fd25f4
megagao <megagao@126.com>
2016-11-26 14:38:12 +0800

    添加了工艺要求PO对象

---
+++ src/main/java/org/hqu/production_ms/domain/po/TechnologyRequirementPO.java
@@ -0,0 +1,17 @@
+package org.hqu.production_ms.domain.po;
+
+import org.hqu.production_ms.domain.TechnologyRequirement;
+
+public class TechnologyRequirementPO extends TechnologyRequirement{
+	
+	private String technologyName;
+
+	public String getTechnologyName() {
+		return technologyName;
+	}
+
+	public void setTechnologyName(String technologyName) {
+		this.technologyName = technologyName;
+	}
+	
+}
