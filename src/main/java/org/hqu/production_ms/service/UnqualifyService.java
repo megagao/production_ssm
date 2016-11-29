3b1e0722c7085ed904782022ea76e5268606ee34
megagao <megagao@126.com>
2016-11-29 21:57:06 +0800

    添加了不合格品service

---
+++ src/main/java/org/hqu/production_ms/service/UnqualifyService.java
@@ -0,0 +1,30 @@
+package org.hqu.production_ms.service;
+
+
+
+import org.hqu.production_ms.domain.UnqualifyApply;
+import org.hqu.production_ms.domain.custom.CustomResult;
+import org.hqu.production_ms.domain.custom.EUDataGridResult;
+
+public interface UnqualifyService {
+
+	EUDataGridResult getList(Integer page, Integer rows, UnqualifyApply unqualifyApply);
+
+	EUDataGridResult searchUnqualifyByUnqualifyId(int page, int rows, String unqualifyId);
+	
+	UnqualifyApply get(String string);
+	
+	CustomResult delete(String string);
+
+	CustomResult deleteBatch(String[] ids);
+
+	CustomResult insert(UnqualifyApply unqualify);
+
+	//更新部分字段，用的是updateSelective判断非空的字段进行更新
+    CustomResult update(UnqualifyApply unqualify);
+    
+    //更新全部字段，不判断非空，直接进行更新
+    CustomResult updateAll(UnqualifyApply unqualify);
+    
+    CustomResult updateNote(UnqualifyApply unqualify);
+}
