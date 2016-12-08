1ed05169871fcfdb6952e6db4873f131e3802c33
megagao <megagao@126.com>
2016-12-08 21:46:26 +0800

    添加了统一异常处理，方法向上抛异常

--- src/main/java/org/hqu/production_ms/mapper/ProcessMapper.java
+++ src/main/java/org/hqu/production_ms/mapper/ProcessMapper.java
@@ -5,9 +5,15 @@ import java.util.List;
 import org.apache.ibatis.annotations.Param;
 import org.hqu.production_ms.domain.Process;
 import org.hqu.production_ms.domain.ProcessExample;
+import org.hqu.production_ms.domain.TechnologyPlan;
 
 public interface ProcessMapper {
-    int countByExample(ProcessExample example);
+	//扩展的mapper接口方法
+	List<TechnologyPlan> find(TechnologyPlan technologyPlan);
+	
+	int deleteBatch(String[] ids);
+	//
+	int countByExample(ProcessExample example);
 
     int deleteByExample(ProcessExample example);
 
@@ -28,4 +34,8 @@ public interface ProcessMapper {
     int updateByPrimaryKeySelective(Process record);
 
     int updateByPrimaryKey(Process record);
+
+	List<Process> searchProcessByProcessId(String processId);
+
+	List<Process> searchProcessByTechnologyPlanId(String technologyPlanId);
 }
