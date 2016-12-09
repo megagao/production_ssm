c93eb7f0e7961ca081a58061010e43670733a664
megagao <megagao@126.com>
2016-12-09 10:10:32 +0800

    添加了hibernate validator校验

--- src/main/java/org/hqu/production_ms/controller/CustomController.java
+++ src/main/java/org/hqu/production_ms/controller/CustomController.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.validation.Valid;
+
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.subject.Subject;
 import org.hqu.production_ms.domain.Custom;
@@ -13,6 +15,8 @@ import org.hqu.production_ms.domain.custom.EUDataGridResult;
 import org.hqu.production_ms.service.CustomService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -27,20 +31,20 @@ public class CustomController {
 	
 	@RequestMapping("/get/{customId}")
 	@ResponseBody
-	public Custom getItemById(@PathVariable String customId) {
+	public Custom getItemById(@PathVariable String customId) throws Exception{
 		Custom custom = customService.get(customId);
 		return custom;
 	}
 	
 	
 	@RequestMapping("/find")
-	public String find() {
+	public String find() throws Exception{
 		return "custom_list";
 	}
 	
 	@RequestMapping("/add_judge")
 	@ResponseBody
-	public Map<String,Object> customAddJudge() {
+	public Map<String,Object> customAddJudge() throws Exception{
 		//从shiro的session中取activeUser
 		Subject subject = SecurityUtils.getSubject();
 		//取身份信息
@@ -65,7 +69,7 @@ public class CustomController {
 	
 	@RequestMapping("/edit_judge")
 	@ResponseBody
-	public Map<String,Object> customEditJudge() {
+	public Map<String,Object> customEditJudge() throws Exception{
 		Subject subject = SecurityUtils.getSubject();
 		ActiveUser activeUser = (ActiveUser) subject.getPrincipal();
 		Map<String,Object> map = new HashMap<String,Object>();
@@ -82,28 +86,32 @@ public class CustomController {
 	}
 	
 	@RequestMapping("/edit")
-	public String edit() {
+	public String edit() throws Exception{
 		return "custom_edit";
 	}
 	
 	@RequestMapping("/get_data")
 	@ResponseBody
-	public List<Custom> getData() {
+	public List<Custom> getData() throws Exception{
 		 List<Custom> list = customService.find();
 		return list;
 	}
 	
 	@RequestMapping("/list")
 	@ResponseBody
-	public EUDataGridResult getItemList(Integer page, Integer rows, Custom custom) {
+	public EUDataGridResult getItemList(Integer page, Integer rows, Custom custom) throws Exception{
 		EUDataGridResult result = customService.getList(page, rows, custom);
 		return result;
 	}
 	
 	@RequestMapping(value="/insert", method=RequestMethod.POST)
 	@ResponseBody
-	private CustomResult insert(Custom custom) throws Exception {
+	private CustomResult insert(@Valid Custom custom, BindingResult bindingResult) throws Exception {
 		CustomResult result;
+		if(bindingResult.hasErrors()){
+			FieldError fieldError = bindingResult.getFieldError();
+			return CustomResult.build(100, fieldError.getDefaultMessage());
+		}
 		if(customService.get(custom.getCustomId()) != null){
 			result = new CustomResult(0, "该客户编号已经存在，请更换客户编号！", null);
 		}else{
@@ -114,28 +122,37 @@ public class CustomController {
 	
 	@RequestMapping(value="/update")
 	@ResponseBody
-	private CustomResult update(Custom custom) throws Exception {
-		CustomResult result = customService.update(custom);
-		return result;
+	private CustomResult update(@Valid Custom custom, BindingResult bindingResult) throws Exception {
+		if(bindingResult.hasErrors()){
+			FieldError fieldError = bindingResult.getFieldError();
+			return CustomResult.build(100, fieldError.getDefaultMessage());
+		}
+		return customService.update(custom);
 	}
 	
 	@RequestMapping(value="/update_all")
 	@ResponseBody
-	private CustomResult updateAll(Custom custom) throws Exception {
-		CustomResult result = customService.updateAll(custom);
-		return result;
+	private CustomResult updateAll(@Valid Custom custom, BindingResult bindingResult) throws Exception {
+		if(bindingResult.hasErrors()){
+			FieldError fieldError = bindingResult.getFieldError();
+			return CustomResult.build(100, fieldError.getDefaultMessage());
+		}
+		return customService.updateAll(custom);
 	}
 	
 	@RequestMapping(value="/update_note")
 	@ResponseBody
-	private CustomResult updateNote(Custom custom) throws Exception {
-		CustomResult result = customService.updateNote(custom);
-		return result;
+	private CustomResult updateNote(@Valid Custom custom, BindingResult bindingResult) throws Exception {
+		if(bindingResult.hasErrors()){
+			FieldError fieldError = bindingResult.getFieldError();
+			return CustomResult.build(100, fieldError.getDefaultMessage());
+		}
+		return customService.updateNote(custom);
 	}
 	
 	@RequestMapping("/delete_judge")
 	@ResponseBody
-	public Map<String,Object> customDeleteJudge() {
+	public Map<String,Object> customDeleteJudge() throws Exception{
 		Subject subject = SecurityUtils.getSubject();
 		ActiveUser activeUser = (ActiveUser) subject.getPrincipal();
 		Map<String,Object> map = new HashMap<String,Object>();
@@ -166,8 +183,24 @@ public class CustomController {
 	
 	@RequestMapping(value="/change_status")
 	@ResponseBody
-	public CustomResult changeStatus(String[] ids) {
+	public CustomResult changeStatus(String[] ids) throws Exception{
 		CustomResult result = customService.changeStatus(ids);
+		return result;
+	}
+	
+	//搜索
+	@RequestMapping("/search_custom_by_customId")
+	@ResponseBody
+	public EUDataGridResult searchCustomByCustomId(Integer page, Integer rows, String searchValue) throws Exception{
+		EUDataGridResult result = customService.searchCustomByCustomId(page, rows, searchValue);
+		return result;
+	}
+	
+	//搜索
+	@RequestMapping("/search_custom_by_customName")
+	@ResponseBody
+	public EUDataGridResult searchCustomByCustomName(Integer page, Integer rows, String searchValue) throws Exception{
+		EUDataGridResult result = customService.searchCustomByCustomName(page, rows, searchValue);
 		return result;
 	}
 }
