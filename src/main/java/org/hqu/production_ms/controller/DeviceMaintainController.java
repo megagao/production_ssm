b98695509bb8e09430b36a26177de76f61e791f7
megagao <megagao@126.com>
2016-12-09 10:08:48 +0800

    删除了多余的引入包

--- src/main/java/org/hqu/production_ms/controller/DeviceMaintainController.java
+++ src/main/java/org/hqu/production_ms/controller/DeviceMaintainController.java
@@ -1,19 +1,18 @@
 package org.hqu.production_ms.controller;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
 import org.hqu.production_ms.domain.DeviceMaintain;
+import org.hqu.production_ms.domain.custom.ActiveUser;
 import org.hqu.production_ms.domain.custom.CustomResult;
 import org.hqu.production_ms.domain.custom.EUDataGridResult;
 import org.hqu.production_ms.service.DeviceMaintainService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.ServletRequestDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -25,24 +24,76 @@ public class DeviceMaintainController {
 	@Autowired
 	private DeviceMaintainService deviceMaintainService;
 	
-	@InitBinder
-    public void InitBinder(HttpServletRequest request,
-            ServletRequestDataBinder binder) {
-        // 不要删除下行注释!!! 将来"yyyy-MM-dd"将配置到properties文件中
-        // SimpleDateFormat dateFormat = new
-        // SimpleDateFormat(getText("date.format", request.getLocale()));
-        SimpleDateFormat dateFormat = new SimpleDateFormat(
-                "yyyy-MM-dd HH:mm:ss");
-        dateFormat.setLenient(false);
-        binder.registerCustomEditor(Date.class, null, new CustomDateEditor(
-                dateFormat, true));
-    }
-
 	@RequestMapping("/list")
 	@ResponseBody
-	public EUDataGridResult getListType(Integer page, Integer rows, DeviceMaintain deviceMaintain) {
+	public EUDataGridResult getListType(Integer page, Integer rows, DeviceMaintain deviceMaintain) throws Exception{
 		EUDataGridResult result = deviceMaintainService.getList(page, rows, deviceMaintain);
 		return result;
+	}
+	
+	@RequestMapping("/add")
+	public String add() throws Exception{
+		return "deviceMaintain_add";
+	}
+	
+	@RequestMapping("/add_judge")
+	@ResponseBody
+	public Map<String,Object> deviceMaintainAddJudge() throws Exception{
+		//从shiro的session中取activeUser
+		Subject subject = SecurityUtils.getSubject();
+		//取身份信息
+		ActiveUser activeUser = (ActiveUser) subject.getPrincipal();
+		Map<String,Object> map = new HashMap<String,Object>(); 
+		if(!activeUser.getUserStatus().equals("1")){
+			map.put("msg", "您的账户已被锁定，请切换账户登录！");
+		}else if(!activeUser.getRoleStatus().equals("1")){
+			map.put("msg", "当前角色已被锁定，请切换账户登录！");
+		}else{
+			if(!subject.isPermitted("custom:add")){
+				map.put("msg", "您没有权限，请切换用户登录！");
+			}
+		}
+		return map;
+	}
+	
+	@RequestMapping("/edit")
+	public String edit() throws Exception{
+		return "deviceMaintain_edit";
+	}
+	
+	@RequestMapping("/edit_judge")
+	@ResponseBody
+	public Map<String,Object> deviceMaintainEditJudge() throws Exception{
+		Subject subject = SecurityUtils.getSubject();
+		ActiveUser activeUser = (ActiveUser) subject.getPrincipal();
+		Map<String,Object> map = new HashMap<String,Object>();
+		if(!activeUser.getUserStatus().equals("1")){
+			map.put("msg", "您的账户已被锁定，请切换账户登录！");
+		}else if(!activeUser.getRoleStatus().equals("1")){
+			map.put("msg", "当前角色已被锁定，请切换账户登录！");
+		}else{
+			if(!subject.isPermitted("process:edit")){
+				map.put("msg", "您没有权限，请切换用户登录！");
+			}
+		}
+		return map;
+	}
+	
+	@RequestMapping("/delete_judge")
+	@ResponseBody
+	public Map<String,Object> deviceMaintainDeleteJudge() throws Exception{
+		Subject subject = SecurityUtils.getSubject();
+		ActiveUser activeUser = (ActiveUser) subject.getPrincipal();
+		Map<String,Object> map = new HashMap<String,Object>();
+		if(!activeUser.getUserStatus().equals("1")){
+			map.put("msg", "您的账户已被锁定，请切换账户登录！");
+		}else if(!activeUser.getRoleStatus().equals("1")){
+			map.put("msg", "当前角色已被锁定，请切换账户登录！");
+			if(!subject.isPermitted("custom:delete")){
+				map.put("msg", "您没有权限，请切换用户登录！");
+			}
+		}
+		return map;
 	}
 	
 	/*
@@ -54,14 +105,12 @@ public class DeviceMaintainController {
 	@RequestMapping(value="/insert", method=RequestMethod.POST)
 	@ResponseBody
 	private CustomResult insert(DeviceMaintain deviceMaintain) throws Exception {
-		CustomResult result = deviceMaintainService.insert(deviceMaintain);
-		return result;
-	}
-	
-	@RequestMapping(value="/delete")
-	@ResponseBody
-	private CustomResult delete(String deviceMaintainId) throws Exception {
-		CustomResult result = deviceMaintainService.delete(deviceMaintainId);
+		CustomResult result;
+		if(deviceMaintainService.get(deviceMaintain.getDeviceMaintainId()) != null){
+			result = new CustomResult(0, "该设备维修编号已经存在，请更换设备维修编号！", null);
+		}else{
+			result = deviceMaintainService.insert(deviceMaintain);
+		}
 		return result;
 	}
 	
@@ -69,6 +118,20 @@ public class DeviceMaintainController {
 	@ResponseBody
 	private CustomResult update(DeviceMaintain deviceMaintain) throws Exception {
 		CustomResult result = deviceMaintainService.update(deviceMaintain);
+		return result;
+	}
+	
+	@RequestMapping(value="/delete_batch")
+	@ResponseBody
+	private CustomResult deleteBatch(String[] ids) throws Exception {
+		CustomResult result = deviceMaintainService.deleteBatch(ids);
+		return result;
+	}
+	
+	@RequestMapping(value="/update_note")
+	@ResponseBody
+	private CustomResult updateNote(DeviceMaintain deviceMaintain) throws Exception {
+		CustomResult result = deviceMaintainService.updateNote(deviceMaintain);
 		return result;
 	}
 }
