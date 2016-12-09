631a6d93e61dd2000a721afbd2b9cfef55da4633
megagao <megagao@126.com>
2016-12-09 10:11:40 +0800

    添加了统一异常处理，方法向上抛异常

--- src/main/java/org/hqu/production_ms/controller/OrderController.java
+++ src/main/java/org/hqu/production_ms/controller/OrderController.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.validation.Valid;
+
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.subject.Subject;
 import org.hqu.production_ms.domain.COrder;
@@ -14,6 +16,8 @@ import org.hqu.production_ms.domain.po.COrderPO;
 import org.hqu.production_ms.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -28,26 +32,26 @@ public class OrderController {
 	
 	@RequestMapping("/get/{orderId}")
 	@ResponseBody
-	public COrder getItemById(@PathVariable String orderId) {
+	public COrder getItemById(@PathVariable String orderId) throws Exception{
 		COrder cOrder = orderService.get(orderId);
 		return cOrder;
 	}
 	
 	@RequestMapping("/get_data")
 	@ResponseBody
-	public List<COrder> getData() {
+	public List<COrder> getData() throws Exception{
 		 List<COrder> list = orderService.find();
 		return list;
 	}
 	
 	@RequestMapping("/find")
-	public String find() {
-		return "order_list";
+	public String find() throws Exception{
+		return "order_list";                                                                                                                                                                                                                                                                                                 
 	}
 	
 	@RequestMapping("/add_judge")
 	@ResponseBody
-	public Map<String,Object> orderAddJudge() {
+	public Map<String,Object> orderAddJudge() throws Exception{
 		//从shiro的session中取activeUser
 		Subject subject = SecurityUtils.getSubject();
 		//取身份信息
@@ -66,13 +70,13 @@ public class OrderController {
 	}
 	
 	@RequestMapping("/add")
-	public String add() {
+	public String add() throws Exception{
 		return "order_add";
 	}
 	
 	@RequestMapping("/edit_judge")
 	@ResponseBody
-	public Map<String,Object> orderEditJudge() {
+	public Map<String,Object> orderEditJudge() throws Exception{
 		Subject subject = SecurityUtils.getSubject();
 		ActiveUser activeUser = (ActiveUser) subject.getPrincipal();
 		Map<String,Object> map = new HashMap<String,Object>();
@@ -89,21 +93,25 @@ public class OrderController {
 	}
 	
 	@RequestMapping("/edit")
-	public String edit() {
+	public String edit() throws Exception{
 		return "order_edit";
 	}
 	
 	@RequestMapping("/list")
 	@ResponseBody
-	public EUDataGridResult getList(Integer page, Integer rows, COrder cOrder) {
+	public EUDataGridResult getList(Integer page, Integer rows, COrder cOrder) throws Exception{
 		EUDataGridResult result = orderService.getList(page, rows, cOrder);
 		return result;
 	}
 	
 	@RequestMapping(value="/insert", method=RequestMethod.POST)
 	@ResponseBody
-	private CustomResult insert(COrderPO cOrder) throws Exception {
+	private CustomResult insert(@Valid COrderPO cOrder, BindingResult bindingResult) throws Exception {
 		CustomResult result;
+		if(bindingResult.hasErrors()){
+			FieldError fieldError = bindingResult.getFieldError();
+			return CustomResult.build(100, fieldError.getDefaultMessage());
+		}
 		if(orderService.get(cOrder.getOrderId()) != null){
 			result = new CustomResult(0, "该订单编号已经存在，请更换订单编号！", null);
 		}else{
@@ -114,28 +122,37 @@ public class OrderController {
 	
 	@RequestMapping(value="/update")
 	@ResponseBody
-	private CustomResult update(COrderPO cOrder) throws Exception {
-		CustomResult result = orderService.update(cOrder);
-		return result;
+	private CustomResult update(@Valid COrderPO cOrder, BindingResult bindingResult) throws Exception {
+		if(bindingResult.hasErrors()){
+			FieldError fieldError = bindingResult.getFieldError();
+			return CustomResult.build(100, fieldError.getDefaultMessage());
+		}
+		return orderService.update(cOrder);
 	}
 	
 	@RequestMapping(value="/update_all")
 	@ResponseBody
-	private CustomResult updateAll(COrderPO cOrder) throws Exception {
-		CustomResult result = orderService.updateAll(cOrder);
-		return result;
+	private CustomResult updateAll(@Valid COrderPO cOrder, BindingResult bindingResult) throws Exception {
+		if(bindingResult.hasErrors()){
+			FieldError fieldError = bindingResult.getFieldError();
+			return CustomResult.build(100, fieldError.getDefaultMessage());
+		}
+		return orderService.updateAll(cOrder);
 	}
 	
 	@RequestMapping(value="/update_note")
 	@ResponseBody
-	private CustomResult updateNote(COrderPO cOrder) throws Exception {
-		CustomResult result = orderService.updateNote(cOrder);
-		return result;
+	private CustomResult updateNote(@Valid COrderPO cOrder, BindingResult bindingResult) throws Exception {
+		if(bindingResult.hasErrors()){
+			FieldError fieldError = bindingResult.getFieldError();
+			return CustomResult.build(100, fieldError.getDefaultMessage());
+		}
+		return orderService.updateNote(cOrder);
 	}
 	
 	@RequestMapping("/delete_judge")
 	@ResponseBody
-	public Map<String,Object> orderDeleteJudge() {
+	public Map<String,Object> orderDeleteJudge() throws Exception{
 		Subject subject = SecurityUtils.getSubject();
 		ActiveUser activeUser = (ActiveUser) subject.getPrincipal();
 		Map<String,Object> map = new HashMap<String,Object>();
@@ -161,15 +178,41 @@ public class OrderController {
 	@RequestMapping(value="/delete_batch")
 	@ResponseBody
 	private CustomResult deleteBatch(String[] ids) throws Exception {
-		System.out.println(ids);
 		CustomResult result = orderService.deleteBatch(ids);
 		return result;
 	}
 	
 	@RequestMapping(value="/change_status")
 	@ResponseBody
-	public CustomResult changeStatus(String[] ids) {
+	public CustomResult changeStatus(String[] ids) throws Exception{
 		CustomResult result = orderService.changeStatus(ids);
+		return result;
+	}
+	
+	//搜索
+	@RequestMapping("/search_order_by_orderId")
+	@ResponseBody
+	public EUDataGridResult searchOrderByOrderId(Integer page, Integer rows, String searchValue) 
+			throws Exception{
+		EUDataGridResult result = orderService.searchOrderByOrderId(page, rows, searchValue);
+		return result;
+	}
+	
+	//搜索
+	@RequestMapping("/search_order_by_orderCustom")
+	@ResponseBody
+	public EUDataGridResult searchOrderByOrderCustom(Integer page, Integer rows, String searchValue) 
+			throws Exception{
+		EUDataGridResult result = orderService.searchOrderByCustomName(page, rows, searchValue);
+		return result;
+	}
+	
+	//搜索
+	@RequestMapping("/search_order_by_orderProduct")
+	@ResponseBody
+	public EUDataGridResult searchOrderByProductName(Integer page, Integer rows, String searchValue) 
+			throws Exception{
+		EUDataGridResult result = orderService.searchOrderByProductName(page, rows, searchValue);
 		return result;
 	}
 }
