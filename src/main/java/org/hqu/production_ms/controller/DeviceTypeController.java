6d6e025a9711666f472d099daf7e19ab8ad2687c
megagao <megagao@126.com>
2016-12-09 10:09:14 +0800

    添加了统一异常管理，方法向上抛异常

--- src/main/java/org/hqu/production_ms/controller/DeviceTypeController.java
+++ src/main/java/org/hqu/production_ms/controller/DeviceTypeController.java
@@ -2,19 +2,28 @@ package org.hqu.production_ms.controller;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
+import org.hqu.production_ms.domain.COrder;
 import org.hqu.production_ms.domain.DeviceType;
+import org.hqu.production_ms.domain.Process;
+import org.hqu.production_ms.domain.custom.ActiveUser;
 import org.hqu.production_ms.domain.custom.CustomResult;
 import org.hqu.production_ms.domain.custom.EUDataGridResult;
 import org.hqu.production_ms.service.DeviceTypeService;
+import org.hqu.production_ms.util.JsonUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.ServletRequestDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -26,31 +35,90 @@ public class DeviceTypeController {
 	@Autowired
 	private DeviceTypeService deviceTypeService;
 	
-	@InitBinder
-    public void InitBinder(HttpServletRequest request,
-            ServletRequestDataBinder binder) {
-        // 不要删除下行注释!!! 将来"yyyy-MM-dd"将配置到properties文件中
-        // SimpleDateFormat dateFormat = new
-        // SimpleDateFormat(getText("date.format", request.getLocale()));
-        SimpleDateFormat dateFormat = new SimpleDateFormat(
-                "yyyy-MM-dd HH:mm:ss");
-        dateFormat.setLenient(false);
-        binder.registerCustomEditor(Date.class, null, new CustomDateEditor(
-                dateFormat, true));
-    }
-
 	@RequestMapping("/list")
 	@ResponseBody
-	public EUDataGridResult getListType(Integer page, Integer rows, DeviceType deviceType) {
+	public EUDataGridResult getListType(Integer page, Integer rows, DeviceType deviceType) throws Exception{
 		EUDataGridResult result = deviceTypeService.getList(page, rows, deviceType);
 		return result;
 	}
 	
-	@RequestMapping("/list_type")
+	@RequestMapping("/get/{orderId}")
 	@ResponseBody
-	public List<?> getList_Type() {
-	EUDataGridResult result = deviceTypeService.getList_Type();
-		return result.getRows();
+	public DeviceType getItemById(@PathVariable String orderId) throws Exception{
+		DeviceType device = deviceTypeService.get(orderId);
+		return device;
+	}
+	
+	@RequestMapping("/get_data")
+	@ResponseBody
+	public List<DeviceType> getData() throws Exception{
+		List<DeviceType> list = deviceTypeService.find();
+		return list;
+	}
+	
+	@RequestMapping("/add")
+	public String add() throws Exception{
+		return "deviceType_add";
+	}
+	
+	@RequestMapping("/add_judge")
+	@ResponseBody
+	public Map<String,Object> deviceTypeAddJudge() throws Exception{
+		//从shiro的session中取activeUser
+		Subject subject = SecurityUtils.getSubject();
+		//取身份信息
+		ActiveUser activeUser = (ActiveUser) subject.getPrincipal();
+		Map<String,Object> map = new HashMap<String,Object>(); 
+		if(!activeUser.getUserStatus().equals("1")){
+			map.put("msg", "您的账户已被锁定，请切换账户登录！");
+		}else if(!activeUser.getRoleStatus().equals("1")){
+			map.put("msg", "当前角色已被锁定，请切换账户登录！");
+		}else{
+			if(!subject.isPermitted("custom:add")){
+				map.put("msg", "您没有权限，请切换用户登录！");
+			}
+		}
+		return map;
+	}
+	
+	@RequestMapping("/edit")
+	public String edit() throws Exception{
+		return "deviceType_edit";
+	}
+	
+	@RequestMapping("/edit_judge")
+	@ResponseBody
+	public Map<String,Object> deviceTypeEditJudge() throws Exception{
+		Subject subject = SecurityUtils.getSubject();
+		ActiveUser activeUser = (ActiveUser) subject.getPrincipal();
+		Map<String,Object> map = new HashMap<String,Object>();
+		if(!activeUser.getUserStatus().equals("1")){
+			map.put("msg", "您的账户已被锁定，请切换账户登录！");
+		}else if(!activeUser.getRoleStatus().equals("1")){
+			map.put("msg", "当前角色已被锁定，请切换账户登录！");
+		}else{
+			if(!subject.isPermitted("process:edit")){
+				map.put("msg", "您没有权限，请切换用户登录！");
+			}
+		}
+		return map;
+	}
+	
+	@RequestMapping("/delete_judge")
+	@ResponseBody
+	public Map<String,Object> deviceTypeDeleteJudge() throws Exception{
+		Subject subject = SecurityUtils.getSubject();
+		ActiveUser activeUser = (ActiveUser) subject.getPrincipal();
+		Map<String,Object> map = new HashMap<String,Object>();
+		if(!activeUser.getUserStatus().equals("1")){
+			map.put("msg", "您的账户已被锁定，请切换账户登录！");
+		}else if(!activeUser.getRoleStatus().equals("1")){
+			map.put("msg", "当前角色已被锁定，请切换账户登录！");
+			if(!subject.isPermitted("custom:delete")){
+				map.put("msg", "您没有权限，请切换用户登录！");
+			}
+		}
+		return map;
 	}
 	
 	/*
@@ -62,14 +130,12 @@ public class DeviceTypeController {
 	@RequestMapping(value="/insert", method=RequestMethod.POST)
 	@ResponseBody
 	private CustomResult insert(DeviceType deviceType) throws Exception {
-		CustomResult result = deviceTypeService.insert(deviceType);
-		return result;
-	}
-	
-	@RequestMapping(value="/delete")
-	@ResponseBody
-	private CustomResult delete(String deviceTypeId) throws Exception {
-		CustomResult result = deviceTypeService.delete(deviceTypeId);
+		CustomResult result;
+		if(deviceTypeService.get(deviceType.getDeviceTypeId()) != null){
+			result = new CustomResult(0, "该设备种类编号已经存在，请更换设备种类编号！", null);
+		}else{
+			result = deviceTypeService.insert(deviceType);
+		}
 		return result;
 	}
 	
@@ -77,6 +143,21 @@ public class DeviceTypeController {
 	@ResponseBody
 	private CustomResult update(DeviceType deviceType) throws Exception {
 		CustomResult result = deviceTypeService.update(deviceType);
+		return result;
+	}
+
+	@RequestMapping(value="/delete_batch")
+	@ResponseBody
+	private CustomResult deleteBatch(String[] ids) throws Exception {
+		CustomResult result = deviceTypeService.deleteBatch(ids);
+		return result;
+	}
+	
+	
+	@RequestMapping(value="/update_all")
+	@ResponseBody
+	private CustomResult updateAll(DeviceType deviceType) throws Exception {
+		CustomResult result = deviceTypeService.updateAll(deviceType);
 		return result;
 	}
 }
