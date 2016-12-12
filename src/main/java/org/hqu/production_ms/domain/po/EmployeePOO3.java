50a5568857354e57ddcdfa8e69a0ea40fe3c29d1
megagao <megagao@126.com>
2016-12-12 14:13:57 +0800

    添加了pojo

---
+++ src/main/java/org/hqu/production_ms/domain/po/EmployeePOO3.java
@@ -0,0 +1,26 @@
+package org.hqu.production_ms.domain.po;
+
+
+public class EmployeePOO3 {
+    
+    private String deviceMaintainEmpId;
+
+    private String deviceMaintainEmp;
+
+	public String getDeviceMaintainEmpId() {
+		return deviceMaintainEmpId;
+	}
+
+	public void setDeviceMaintainEmpId(String deviceMaintainEmpId) {
+		this.deviceMaintainEmpId = deviceMaintainEmpId;
+	}
+
+	public String getDeviceMaintainEmp() {
+		return deviceMaintainEmp;
+	}
+
+	public void setDeviceMaintainEmp(String deviceMaintainEmp) {
+		this.deviceMaintainEmp = deviceMaintainEmp;
+	}
+
+}
