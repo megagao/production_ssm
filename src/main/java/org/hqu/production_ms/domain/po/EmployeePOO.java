3dfb11fbd53705b5c6524ea8cd09272bc88dab8a
megagao <megagao@126.com>
2016-12-12 14:13:30 +0800

    添加了封装类

---
+++ src/main/java/org/hqu/production_ms/domain/po/EmployeePOO.java
@@ -0,0 +1,27 @@
+package org.hqu.production_ms.domain.po;
+
+
+public class EmployeePOO {
+    
+    private String deviceKeeperId;
+
+    private String deviceKeeper;
+
+	public String getDeviceKeeperId() {
+		return deviceKeeperId;
+	}
+
+	public void setDeviceKeeperId(String deviceKeeperId) {
+		this.deviceKeeperId = deviceKeeperId;
+	}
+
+	public String getDeviceKeeper() {
+		return deviceKeeper;
+	}
+
+	public void setDeviceKeeper(String deviceKeeper) {
+		this.deviceKeeper = deviceKeeper;
+	}
+    
+    
+}
