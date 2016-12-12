df04d005e82d1d2bc26fd37ab2fb3d8e41274199
megagao <megagao@126.com>
2016-12-12 14:13:41 +0800

    添加了封装对象

---
+++ src/main/java/org/hqu/production_ms/domain/po/EmployeePOO2.java
@@ -0,0 +1,27 @@
+package org.hqu.production_ms.domain.po;
+
+
+public class EmployeePOO2 {
+    
+    private String deviceCheckEmpId;
+
+    private String deviceCheckEmp;
+
+	public String getDeviceCheckEmpId() {
+		return deviceCheckEmpId;
+	}
+
+	public void setDeviceCheckEmpId(String deviceCheckEmpId) {
+		this.deviceCheckEmpId = deviceCheckEmpId;
+	}
+
+	public String getDeviceCheckEmp() {
+		return deviceCheckEmp;
+	}
+
+	public void setDeviceCheckEmp(String deviceCheckEmp) {
+		this.deviceCheckEmp = deviceCheckEmp;
+	}
+
+    
+}
