f578845e60bbfa1289303eae91334209bd50cf79
megagao <megagao@126.com>
2016-12-14 12:56:53 +0800

    添加了统一异常处理，方法向上抛异常

--- src/main/java/org/hqu/production_ms/controller/DepartmentController.java
+++ src/main/java/org/hqu/production_ms/controller/DepartmentController.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.validation.Valid;
+
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.subject.Subject;
 import org.hqu.production_ms.domain.Department;
@@ -13,6 +15,8 @@ import org.hqu.production_ms.domain.custom.EUDataGridResult;
 import org.hqu.production_ms.service.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -27,25 +31,25 @@ public class DepartmentController {
 	
 	@RequestMapping("/get/{departmentId}")
 	@ResponseBody
-	public Department getItemById(@PathVariable String departmentId) {
+	public Department getItemById(@PathVariable String departmentId) throws Exception{
 		Department department = departmentService.get(departmentId);
 		return department;
 	}
 	
 	@RequestMapping("/find")
-	public String find() {
+	public String find() throws Exception{
 		return "department_list";
 	}
 	
 	@RequestMapping("/get_data")
 	@ResponseBody
-	public List<Department> getData() {
+	public List<Department> getData() throws Exception{
 		return departmentService.find();
 	}
 	
 	@RequestMapping("/add_judge")
 	@ResponseBody
-	public Map<String,Object> departmentAddJudge() {
+	public Map<String,Object> departmentAddJudge() throws Exception{
 		//从shiro的session中取activeUser
 		Subject subject = SecurityUtils.getSubject();
 		//取身份信息
@@ -64,13 +68,13 @@ public class DepartmentController {
 	}
 	
 	@RequestMapping("/add")
-	public String add() {
+	public String add() throws Exception{
 		return "department_add";
 	}
 	
 	@RequestMapping("/edit_judge")
 	@ResponseBody
-	public Map<String,Object> departmentEditJudge() {
+	public Map<String,Object> departmentEditJudge() throws Exception{
 		Subject subject = SecurityUtils.getSubject();
 		ActiveUser activeUser = (ActiveUser) subject.getPrincipal();
 		Map<String,Object> map = new HashMap<String,Object>();
@@ -87,21 +91,25 @@ public class DepartmentController {
 	}
 	
 	@RequestMapping("/edit")
-	public String edit() {
+	public String edit() throws Exception{
 		return "department_edit";
 	}
 	
 	@RequestMapping("/list")
 	@ResponseBody
-	public EUDataGridResult getItemList(Integer page, Integer rows, Department department) {
+	public EUDataGridResult getItemList(Integer page, Integer rows, Department department) throws Exception{
 		EUDataGridResult result = departmentService.getList(page, rows, department);
 		return result;
 	}
 	
 	@RequestMapping(value="/insert", method=RequestMethod.POST)
 	@ResponseBody
-	private CustomResult insert(Department department) throws Exception {
+	private CustomResult insert(@Valid Department department, BindingResult bindingResult) throws Exception {
 		CustomResult result;
+		if(bindingResult.hasErrors()){
+			FieldError fieldError = bindingResult.getFieldError();
+			return CustomResult.build(100, fieldError.getDefaultMessage());
+		}
 		if(departmentService.get(department.getDepartmentId()) != null){
 			result = new CustomResult(0, "该部门编号已经存在，请更换部门编号！", null);
 		}else{
@@ -112,28 +120,37 @@ public class DepartmentController {
 	
 	@RequestMapping(value="/update")
 	@ResponseBody
-	private CustomResult update(Department department) throws Exception {
-		CustomResult result = departmentService.update(department);
-		return result;
+	private CustomResult update(@Valid Department department, BindingResult bindingResult) throws Exception {
+		if(bindingResult.hasErrors()){
+			FieldError fieldError = bindingResult.getFieldError();
+			return CustomResult.build(100, fieldError.getDefaultMessage());
+		}
+		return departmentService.update(department);
 	}
 	
 	@RequestMapping(value="/update_all")
 	@ResponseBody
-	private CustomResult updateAll(Department department) throws Exception {
-		CustomResult result = departmentService.updateAll(department);
-		return result;
+	private CustomResult updateAll(@Valid Department department, BindingResult bindingResult) throws Exception {
+		if(bindingResult.hasErrors()){
+			FieldError fieldError = bindingResult.getFieldError();
+			return CustomResult.build(100, fieldError.getDefaultMessage());
+		}
+		return departmentService.updateAll(department);
 	}
 	
 	@RequestMapping(value="/update_note")
 	@ResponseBody
-	private CustomResult updateNote(Department department) throws Exception {
-		CustomResult result = departmentService.updateNote(department);
-		return result;
+	private CustomResult updateNote(@Valid Department department, BindingResult bindingResult) throws Exception {
+		if(bindingResult.hasErrors()){
+			FieldError fieldError = bindingResult.getFieldError();
+			return CustomResult.build(100, fieldError.getDefaultMessage());
+		}
+		return departmentService.updateNote(department);
 	}
 	
 	@RequestMapping("/delete_judge")
 	@ResponseBody
-	public Map<String,Object> departmentDeleteJudge() {
+	public Map<String,Object> departmentDeleteJudge() throws Exception{
 		Subject subject = SecurityUtils.getSubject();
 		ActiveUser activeUser = (ActiveUser) subject.getPrincipal();
 		Map<String,Object> map = new HashMap<String,Object>();
@@ -166,7 +183,8 @@ public class DepartmentController {
 	//搜索
 	@RequestMapping("/search_department_by_departmentId")
 	@ResponseBody
-	public EUDataGridResult searchDepartmentByDepartmentId(Integer page, Integer rows, String searchValue) {
+	public EUDataGridResult searchDepartmentByDepartmentId(Integer page, Integer rows, String searchValue) 
+			throws Exception{
 		EUDataGridResult result = departmentService.searchDepartmentByDepartmentId(page, rows, searchValue);
 		return result;
 	}
@@ -174,7 +192,8 @@ public class DepartmentController {
 	//搜索
 	@RequestMapping("/search_department_by_departmentName")
 	@ResponseBody
-	public EUDataGridResult searchDepartmentByDepartmentName(Integer page, Integer rows, String searchValue) {
+	public EUDataGridResult searchDepartmentByDepartmentName(Integer page, Integer rows, String searchValue) 
+			throws Exception{
 		EUDataGridResult result = departmentService.searchDepartmentByDepartmentName(page, rows, searchValue);
 		return result;
 	}
