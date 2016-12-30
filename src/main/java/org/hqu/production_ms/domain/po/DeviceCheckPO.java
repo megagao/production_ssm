0e391769b6cc2a83762ec725cee7b85b9301a712
megagao <megagao@126.com>
2016-12-30 12:36:09 +0800

    添加了hibernate validator校验

--- src/main/java/org/hqu/production_ms/domain/po/DeviceCheckPO.java
+++ src/main/java/org/hqu/production_ms/domain/po/DeviceCheckPO.java
@@ -1,9 +1,12 @@
 package org.hqu.production_ms.domain.po;
 
+import javax.validation.constraints.Size;
+
 import org.hqu.production_ms.domain.DeviceCheck;
 
 public class DeviceCheckPO extends DeviceCheck{
 	
+	@Size(max=20, message="设备名称请限制在20个字符内")
 	private String deviceName;
 	
 	private String deviceCheckEmp;
