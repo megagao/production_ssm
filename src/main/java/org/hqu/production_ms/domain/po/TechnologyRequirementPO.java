87ec550bba18dde587255583e0731b1d5bf3a74f
megagao <megagao@126.com>
2016-12-30 12:36:33 +0800

    添加了hibernate validator校验

--- src/main/java/org/hqu/production_ms/domain/po/TechnologyRequirementPO.java
+++ src/main/java/org/hqu/production_ms/domain/po/TechnologyRequirementPO.java
@@ -1,10 +1,13 @@
 package org.hqu.production_ms.domain.po;
 
 
+import javax.validation.constraints.Size;
+
 import org.hqu.production_ms.domain.TechnologyRequirement;
 
 public class TechnologyRequirementPO extends TechnologyRequirement{
 	
+	@Size(max=20, message="工艺名称请限制在20个字符内")
 	private String technologyName;
 
 	public String getTechnologyName() {
