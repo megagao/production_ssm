5fb08b5dabf91f1ec3e29785b42495a29f2b0c27
megagao <megagao@126.com>
2017-01-01 12:35:26 +0800

    添加了扩展的mapper接口方法

--- src/main/java/org/hqu/production_ms/mapper/ProcessCountCheckMapper.java
+++ src/main/java/org/hqu/production_ms/mapper/ProcessCountCheckMapper.java
@@ -8,14 +8,17 @@ import org.hqu.production_ms.domain.ProcessCountCheckExample;
 import org.hqu.production_ms.domain.po.ProcessCountCheckPO;
 
 public interface ProcessCountCheckMapper {
+	
+	//扩展的mapper接口方法
 	int updateNote(ProcessCountCheck processCountCheck);
+	
+	int deleteBatch(String[] ids);
 	
 	List<ProcessCountCheckPO> searchPCountCheckByPCountCheckId(String pCountCheckId);
 	
 	List<ProcessCountCheckPO> find(ProcessCountCheck processCountCheck);
 	
-	int deleteBatch(String[] ids);
-	
+	//逆向工程生成的mapper接口
     int countByExample(ProcessCountCheckExample example);
 
     int deleteByExample(ProcessCountCheckExample example);
