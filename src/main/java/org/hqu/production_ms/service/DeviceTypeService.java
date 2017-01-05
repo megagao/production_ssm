f7946cd409d452f432fc87f59e038f1e96025bbc
megagao <megagao@126.com>
2017-01-05 13:06:07 +0800

    添加了统一异常处理，方法向上抛异常

--- src/main/java/org/hqu/production_ms/service/DeviceTypeService.java
+++ src/main/java/org/hqu/production_ms/service/DeviceTypeService.java
@@ -7,32 +7,25 @@ import org.hqu.production_ms.domain.custom.CustomResult;
 import org.hqu.production_ms.domain.custom.EUDataGridResult;
 
 public interface DeviceTypeService {
-
 	
-	//List
 	EUDataGridResult getList(int page, int rows, DeviceType deviceType) throws Exception;
 	
-	//get
 	DeviceType get(String string) throws Exception;
 	
-	//find
 	List<DeviceType> find() throws Exception;
 	
-	//Insert
 	CustomResult insert(DeviceType deviceType) throws Exception;
 	
-	//Delete
 	CustomResult deleteBatch(String[] deviceTypeIds) throws Exception;
 
-	//Update
     CustomResult update(DeviceType deviceType) throws Exception;
     
     //更新全部字段，不判断非空，直接进行更新
     CustomResult updateAll(DeviceType deviceType) throws Exception;
 
 	EUDataGridResult searchDeviceTypeByDeviceTypeId(Integer page, Integer rows,
-			String deviceTypeId);
+			String deviceTypeId) throws Exception;
 
 	EUDataGridResult searchDeviceTypeByDeviceTypeName(Integer page,
-			Integer rows, String deviceTypeName);
+			Integer rows, String deviceTypeName) throws Exception;
 }
