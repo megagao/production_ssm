6e97c98385df744dca5f66184e1b340f85ca4b87
megagao <megagao@126.com>
2017-01-06 10:35:20 +0800

    修改了代码格式，删去了被注释的无用代码

--- src/main/java/org/hqu/production_ms/service/TechnologyRequirementService.java
+++ src/main/java/org/hqu/production_ms/service/TechnologyRequirementService.java
@@ -11,29 +11,22 @@ public interface TechnologyRequirementService {
 	
 	List<Technology> find() throws Exception;  
 	
-	EUDataGridResult getList(int page, int rows, TechnologyRequirement technologyRequirement) throws Exception;
+	EUDataGridResult getList(int page, int rows, TechnologyRequirement technologyRequirement)
+			throws Exception;
 
 	TechnologyRequirement get(String string) throws Exception;
 	
-	/*
-	CustomResult delete(String string) throws Exception;
-	
-	*/
-
 	CustomResult deleteBatch(String[] ids) throws Exception;
 
 	
 	CustomResult insert(TechnologyRequirement technologyRequirement) throws Exception;
 	
-	/*
-	//更新部分字段，用的是updateSelective判断非空的字段进行更新
-    CustomResult update(Custom custom) throws Exception;
-    */
     //更新全部字段，不判断非空，直接进行更新
     CustomResult updateAll(TechnologyRequirement technologyRequirement) throws Exception;
     
     //更新要求
-    CustomResult updateRequirement(TechnologyRequirement technologyRequirement) throws Exception;
+    CustomResult updateRequirement(TechnologyRequirement technologyRequirement) 
+    		throws Exception;
 
 	EUDataGridResult searchTechnologyRequirementByTechnologyRequirementId(
 			Integer page, Integer rows, String technologyRequirementId) throws Exception;
