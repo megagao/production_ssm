b9d65d3c685650e709744321d17b7242d596ae50
megagao <megagao@126.com>
2017-01-06 10:35:41 +0800

    删去了被注释的无用代码

--- src/main/java/org/hqu/production_ms/service/TechnologyService.java
+++ src/main/java/org/hqu/production_ms/service/TechnologyService.java
@@ -14,29 +14,13 @@ public interface TechnologyService {
 
 	Technology get(String string) throws Exception;
 	
-	/*
-	CustomResult delete(String string) throws Exception;
-	
-	*/
-
 	CustomResult deleteBatch(String[] ids) throws Exception;
 
 	
 	CustomResult insert(Technology technology) throws Exception;
 	
-	/*
-	//更新部分字段，用的是updateSelective判断非空的字段进行更新
-    CustomResult update(Custom custom) throws Exception;
-    */
     //更新全部字段，不判断非空，直接进行更新
     CustomResult updateAll(Technology technology) throws Exception;
-    /*
-    //更新备注
-    CustomResult updateNote(Custom custom) throws Exception;
-
-
-    CustomResult changeStatus(String[] ids) throws Exception;
-    */
 
 	EUDataGridResult searchTechnologyByTechnologyId(Integer page, Integer rows,
 			String technologyId) throws Exception;
