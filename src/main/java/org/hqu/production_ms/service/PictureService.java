5ceaf4e62ef339469ee9ba1667f9f3c9a33feea4
megagao <megagao@126.com>
2017-01-06 10:34:04 +0800

    添加了注释

--- src/main/java/org/hqu/production_ms/service/PictureService.java
+++ src/main/java/org/hqu/production_ms/service/PictureService.java
@@ -6,6 +6,12 @@ import org.springframework.web.multipart.MultipartFile;
 
 public interface PictureService {
 	
+	/**
+	 * 
+	 *
+	 * @param  
+	 * @return
+	 */
 	Map<String,Object> uploadPicture(MultipartFile uploadFile) throws Exception;
 	
 	boolean deleteFile(String picName) throws Exception;
