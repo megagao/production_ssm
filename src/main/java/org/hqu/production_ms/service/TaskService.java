1f483e64ad7b762a8fc48ac4d32000edb1f001ad
megagao <megagao@126.com>
2017-01-06 10:36:51 +0800

    添加了统一异常处理，方法向上抛异常

--- src/main/java/org/hqu/production_ms/service/TaskService.java
+++ src/main/java/org/hqu/production_ms/service/TaskService.java
@@ -8,29 +8,29 @@ import org.hqu.production_ms.domain.custom.EUDataGridResult;
 
 public interface TaskService {
 	
-	List<Task> find();  
+	List<Task> find() throws Exception;  
 	
-	EUDataGridResult getList(int page, int rows, Task task);
+	EUDataGridResult getList(int page, int rows, Task task) throws Exception;
 	
-	Task get(String string);
+	Task get(String string) throws Exception;
 	
-	CustomResult delete(String string);
+	CustomResult delete(String string) throws Exception;
 
-	CustomResult deleteBatch(String[] ids);
+	CustomResult deleteBatch(String[] ids) throws Exception;
 
-	CustomResult insert(Task task);
+	CustomResult insert(Task task) throws Exception;
 
     //更新全部字段，不判断非空，直接进行更新
-    CustomResult updateAll(Task task);
+    CustomResult updateAll(Task task) throws Exception;
     
-    CustomResult update(Task task);
+    CustomResult update(Task task) throws Exception;
     
 	EUDataGridResult searchTaskByTaskId(Integer page, Integer rows,
-			String taskId);
+			String taskId) throws Exception;
 
 	EUDataGridResult searchTaskByTaskWorkId(Integer page, Integer rows,
-			String taskWorkId);
+			String taskWorkId) throws Exception;
 
 	EUDataGridResult searchTaskByTaskManufactureSn(Integer page, Integer rows,
-			String taskManufactureSn);
+			String taskManufactureSn) throws Exception;
 }
