12004b058910012069cd9fa464f5ed86bf5067df
megagao <megagao@126.com>
2017-01-06 10:34:50 +0800

    删去了被注释的无用代码

--- src/main/java/org/hqu/production_ms/service/TechnologyPlanService.java
+++ src/main/java/org/hqu/production_ms/service/TechnologyPlanService.java
@@ -14,20 +14,10 @@ public interface TechnologyPlanService {
 
 	TechnologyPlan get(String string) throws Exception;
 	
-	/*
-	CustomResult delete(String string) throws Exception;
-	
-	*/
-
 	CustomResult deleteBatch(String[] ids) throws Exception;
 
 	
 	CustomResult insert(TechnologyPlan technologyPlan) throws Exception;
-	
-	/*
-	//更新部分字段，用的是updateSelective判断非空的字段进行更新
-    CustomResult update(Custom custom) throws Exception;
-    */
 	
     //更新全部字段，不判断非空，直接进行更新
     CustomResult updateAll(TechnologyPlan technologyPlan) throws Exception;
