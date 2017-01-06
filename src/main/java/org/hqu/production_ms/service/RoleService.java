9a3ae0c8e495a6e9cc5ec5607923c5b29b6b727f
megagao <megagao@126.com>
2017-01-06 10:34:10 +0800

    添加了统一异常处理，方法向上抛异常

--- src/main/java/org/hqu/production_ms/service/RoleService.java
+++ src/main/java/org/hqu/production_ms/service/RoleService.java
@@ -9,35 +9,35 @@ import org.hqu.production_ms.domain.po.RolePO;
 
 public interface RoleService {
 	
-	List<SysRole> find();
+	List<SysRole> find() throws Exception;
 	
-	SysRole findRoleByUserId(String userId);
+	SysRole findRoleByUserId(String userId) throws Exception;
 	
-	EUDataGridResult getList(int page, int rows, SysRole sysRole);
+	EUDataGridResult getList(int page, int rows, SysRole sysRole) throws Exception;
 	
-	List<SysRole> findByRoleNameAndId(String rolename, String id);
+	List<SysRole> findByRoleNameAndId(String rolename, String id) throws Exception;
 	
-	SysRole get(String string);
+	SysRole get(String string) throws Exception;
 	
-	CustomResult delete(String string);
+	CustomResult delete(String string) throws Exception;
 
-	CustomResult deleteBatch(String[] ids);
+	CustomResult deleteBatch(String[] ids) throws Exception;
 
-	CustomResult insert(RolePO role);
+	CustomResult insert(RolePO role) throws Exception;
 
 	//更新部分字段，用的是updateSelective判断非空的字段进行更新
-    CustomResult update(RolePO role);
+    CustomResult update(RolePO role) throws Exception;
     
     //更新全部字段，不判断非空，直接进行更新
-    CustomResult updateAll(RolePO role);
+    CustomResult updateAll(RolePO role) throws Exception;
     
-    List<SysRole> searchSysRoleBySysRoleName(String sysRoleName);
+    List<SysRole> searchSysRoleBySysRoleName(String sysRoleName) throws Exception;
 
-	List<SysRole> searchSysRoleBySysRoleId(String sysRoleId);
+	List<SysRole> searchSysRoleBySysRoleId(String sysRoleId) throws Exception;
 
 	EUDataGridResult searchRoleByRoleId(Integer page, Integer rows,
-			String roleId);
+			String roleId) throws Exception;
 
 	EUDataGridResult searchRoleByRoleName(Integer page, Integer rows,
-			String roleName);
+			String roleName) throws Exception;
 }
