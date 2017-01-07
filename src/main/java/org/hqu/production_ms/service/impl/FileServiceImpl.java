139220a517a4fdc0db6b19053b71c6324734de5c
megagao <megagao@126.com>
2017-01-07 15:22:29 +0800

    修改了代码格式

--- src/main/java/org/hqu/production_ms/service/impl/FileServiceImpl.java
+++ src/main/java/org/hqu/production_ms/service/impl/FileServiceImpl.java
@@ -17,9 +17,7 @@ public class FileServiceImpl implements FileService{
 		
 		Map<String,Object> resultMap = new HashMap<String,Object>();
 		try {
-			
 			if(uploadFile!=null && uploadFile.getOriginalFilename()!=null && uploadFile.getOriginalFilename().length()>0){
-			
 					//生成一个新的文件名
 					//取原始文件名
 					String fileName = uploadFile.getOriginalFilename();
@@ -30,16 +28,13 @@ public class FileServiceImpl implements FileService{
 					//String newName = oldName.substring(0,oldName.lastIndexOf("."))+"("+date+")"+oldName.substring(oldName.lastIndexOf("."));
 					
 					String filePath = "D:\\upload\\temp\\file\\";
-					
 					FileUtil fu = new FileUtil();
 					String newName = fu.newFile(filePath, fileName);
 					
 					//新文件
 					File file = new java.io.File(filePath+newName);
-					
 					//将内存中的文件写入磁盘
 					uploadFile.transferTo(file);
-					
 					//图片上传成功后，将图片的地址写回
 					resultMap.put("error", 0);
 					resultMap.put("url", "/file/" + newName);
@@ -60,14 +55,9 @@ public class FileServiceImpl implements FileService{
 
 	@Override
 	public boolean deleteFile(String fileName) throws Exception{
-		
 		fileName = fileName.substring(fileName.lastIndexOf("/")+1);
-		
 		fileName = "D:\\upload\\temp\\file\\"+fileName;
-		
 		FileUtil.deleteFile(fileName);
-		
 		return true;
 	}
-
 }
