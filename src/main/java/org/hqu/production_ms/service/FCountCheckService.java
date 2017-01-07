5cbaafc86857174c7d8d24d9e910db86dc6c6600
megagao <megagao@126.com>
2017-01-07 15:20:04 +0800

    删除了多余的方法

--- src/main/java/org/hqu/production_ms/service/FCountCheckService.java
+++ src/main/java/org/hqu/production_ms/service/FCountCheckService.java
@@ -1,7 +1,6 @@
 package org.hqu.production_ms.service;
 
 import org.hqu.production_ms.domain.FinalCountCheck;
-import org.hqu.production_ms.domain.UnqualifyApply;
 import org.hqu.production_ms.domain.custom.CustomResult;
 import org.hqu.production_ms.domain.custom.EUDataGridResult;
 
@@ -16,16 +15,11 @@ public interface FCountCheckService {
 
 	CustomResult insert(FinalCountCheck finalCountCheck) throws Exception;
 
-	//更新部分字段，用的是updateSelective判断非空的字段进行更新
-    CustomResult update(UnqualifyApply unqualify) throws Exception;
-    
     //更新全部字段，不判断非空，直接进行更新
     CustomResult updateAll(FinalCountCheck finalCountCheck) throws Exception;
     
     CustomResult updateNote(FinalCountCheck finalCountCheck) throws Exception;
 
-    CustomResult changeStatus(String[] ids) throws Exception;
-    
 	EUDataGridResult searchFCountCheckByOrderId(Integer page, Integer rows,
 			String orderId) throws Exception;
 	
