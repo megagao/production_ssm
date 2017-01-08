41eca760d3f010b92a1f8546e599a1d848a03bfd
megagao <megagao@126.com>
2017-01-08 16:53:24 +0800

    删除了无用的代码

--- src/main/java/org/hqu/production_ms/service/MeasureService.java
+++ src/main/java/org/hqu/production_ms/service/MeasureService.java
@@ -2,7 +2,6 @@ package org.hqu.production_ms.service;
 
 
 import org.hqu.production_ms.domain.FinalMeasuretCheck;
-import org.hqu.production_ms.domain.UnqualifyApply;
 import org.hqu.production_ms.domain.custom.CustomResult;
 import org.hqu.production_ms.domain.custom.EUDataGridResult;
 
@@ -13,21 +12,14 @@ public interface MeasureService {
 
 	FinalMeasuretCheck get(String string) throws Exception;
 	
-	CustomResult delete(String string) throws Exception;
-
 	CustomResult deleteBatch(String[] ids) throws Exception;
 
 	CustomResult insert(FinalMeasuretCheck finalMeasuretCheck) throws Exception;
 
-	//更新部分字段，用的是updateSelective判断非空的字段进行更新
-    CustomResult update(UnqualifyApply unqualify) throws Exception;
-    
     //更新全部字段，不判断非空，直接进行更新
     CustomResult updateAll(FinalMeasuretCheck finalMeasuretCheck) throws Exception;
     
     CustomResult updateNote(FinalMeasuretCheck finalMeasuretCheck) throws Exception;
-
-    CustomResult changeStatus(String[] ids) throws Exception;
 
     EUDataGridResult searchFMeasureCheckByFMeasureCheckId(int page, int rows, 
     		String fMeasureCheckId) throws Exception;
