c523dbf5c17c99c7d28b84d6b3f3303c79334039
megagao <megagao@126.com>
2017-01-08 16:58:03 +0800

    删除了无用的方法

--- src/main/java/org/hqu/production_ms/service/PMeasureCheckService.java
+++ src/main/java/org/hqu/production_ms/service/PMeasureCheckService.java
@@ -1,10 +1,8 @@
 package org.hqu.production_ms.service;
 
-import org.hqu.production_ms.domain.COrder;
 import org.hqu.production_ms.domain.ProcessMeasureCheck;
 import org.hqu.production_ms.domain.custom.CustomResult;
 import org.hqu.production_ms.domain.custom.EUDataGridResult;
-import org.hqu.production_ms.domain.po.COrderPO;
 
 public interface PMeasureCheckService {
 	
@@ -14,23 +12,11 @@ public interface PMeasureCheckService {
 	EUDataGridResult getList(int page, int rows, ProcessMeasureCheck processMeasureCheck) 
 			throws Exception;
 	
-	COrder get(String string) throws Exception;
-	
-	CustomResult delete(String string) throws Exception;
-
 	CustomResult deleteBatch(String[] ids) throws Exception;
 
 	CustomResult insert(ProcessMeasureCheck processMeasureCheck) throws Exception;
 
-	//更新部分字段，用的是updateSelective判断非空的字段进行更新
-    CustomResult update(COrderPO cOrder) throws Exception;
-    
-    //更新全部字段，不判断非空，直接进行更新
-    //CustomResult updateAll(PMeasureCheckService pMeasureCheckService);
+	CustomResult updateAll(ProcessMeasureCheck processMeasureCheck) throws Exception;
     
     CustomResult updateNote(ProcessMeasureCheck processMeasureCheck) throws Exception;
-
-    CustomResult changeStatus(String[] ids) throws Exception;
-
-	CustomResult updateAll(ProcessMeasureCheck processMeasureCheck) throws Exception;
 }
