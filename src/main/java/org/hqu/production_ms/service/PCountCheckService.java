27adbe26a533c49aa00e24a45dbc9f965f7bf661
megagao <megagao@126.com>
2017-01-08 16:55:26 +0800

    删除了无用的代码

--- src/main/java/org/hqu/production_ms/service/PCountCheckService.java
+++ src/main/java/org/hqu/production_ms/service/PCountCheckService.java
@@ -1,33 +1,22 @@
 package org.hqu.production_ms.service;
 
-import org.hqu.production_ms.domain.COrder;
 import org.hqu.production_ms.domain.ProcessCountCheck;
 import org.hqu.production_ms.domain.custom.CustomResult;
 import org.hqu.production_ms.domain.custom.EUDataGridResult;
-import org.hqu.production_ms.domain.po.COrderPO;
 
 public interface PCountCheckService {
 	
 	EUDataGridResult getList(int page, int rows, ProcessCountCheck processCountCheck) throws Exception;
 	
-	COrder get(String string) throws Exception;
-	
-	CustomResult delete(String string) throws Exception;
-
 	CustomResult deleteBatch(String[] ids) throws Exception;
 
 	CustomResult insert(ProcessCountCheck processCountCheck) throws Exception;
 
-	//更新部分字段，用的是updateSelective判断非空的字段进行更新
-    CustomResult update(COrderPO cOrder) throws Exception;
-    
     //更新全部字段，不判断非空，直接进行更新
     CustomResult updateAll(ProcessCountCheck processCountCheck) throws Exception;
     
     CustomResult updateNote(ProcessCountCheck processCountCheck) throws Exception;
 
-    CustomResult changeStatus(String[] ids) throws Exception;
-    
     EUDataGridResult searchPCountCheckByPCountCheckId(int page, int rows, 
 			String pCountCheckId) throws Exception;
 }
