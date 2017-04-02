01bd76d392cd02a8eda30f1592acb1aa0224fb1b
megagao <megagao@126.com>
2017-04-02 21:38:01 +0800

    修改了代码格式

--- src/main/java/org/hqu/production_ms/controller/DeviceCheckController.java
+++ src/main/java/org/hqu/production_ms/controller/DeviceCheckController.java
@@ -1,16 +1,11 @@
 package org.hqu.production_ms.controller;
 
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.validation.Valid;
 
-import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.subject.Subject;
 import org.hqu.production_ms.domain.DeviceCheck;
-import org.hqu.production_ms.domain.custom.ActiveUser;
-import org.hqu.production_ms.domain.custom.CustomResult;
-import org.hqu.production_ms.domain.custom.EUDataGridResult;
+import org.hqu.production_ms.domain.customize.CustomResult;
+import org.hqu.production_ms.domain.customize.EUDataGridResult;
 import org.hqu.production_ms.service.DeviceCheckService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -29,7 +24,8 @@ public class DeviceCheckController {
 	
 	@RequestMapping("/list")
 	@ResponseBody
-	public EUDataGridResult getListType(Integer page, Integer rows, DeviceCheck deviceCheck) throws Exception{
+	public EUDataGridResult getListType(Integer page, Integer rows, DeviceCheck deviceCheck) 
+			throws Exception{
 		EUDataGridResult result = deviceCheckService.getList(page, rows, deviceCheck);
 		return result;
 	}
@@ -39,64 +35,9 @@ public class DeviceCheckController {
 		return "deviceCheck_add";
 	}
 	
-	@RequestMapping("/add_judge")
-	@ResponseBody
-	public Map<String,Object> deviceCheckAddJudge() throws Exception{
-		//从shiro的session中取activeUser
-		Subject subject = SecurityUtils.getSubject();
-		//取身份信息
-		ActiveUser activeUser = (ActiveUser) subject.getPrincipal();
-		Map<String,Object> map = new HashMap<String,Object>(); 
-		if(!activeUser.getUserStatus().equals("1")){
-			map.put("msg", "您的账户已被锁定，请切换账户登录！");
-		}else if(!activeUser.getRoleStatus().equals("1")){
-			map.put("msg", "当前角色已被锁定，请切换账户登录！");
-		}else{
-			if(!subject.isPermitted("deviceCheck:add")){
-				map.put("msg", "您没有权限，请切换用户登录！");
-			}
-		}
-		return map;
-	}
-	
 	@RequestMapping("/edit")
 	public String edit() throws Exception{
 		return "deviceCheck_edit";
-	}
-	
-	@RequestMapping("/edit_judge")
-	@ResponseBody
-	public Map<String,Object> deviceCheckEditJudge() throws Exception{
-		Subject subject = SecurityUtils.getSubject();
-		ActiveUser activeUser = (ActiveUser) subject.getPrincipal();
-		Map<String,Object> map = new HashMap<String,Object>();
-		if(!activeUser.getUserStatus().equals("1")){
-			map.put("msg", "您的账户已被锁定，请切换账户登录！");
-		}else if(!activeUser.getRoleStatus().equals("1")){
-			map.put("msg", "当前角色已被锁定，请切换账户登录！");
-		}else{
-			if(!subject.isPermitted("deviceCheck:edit")){
-				map.put("msg", "您没有权限，请切换用户登录！");
-			}
-		}
-		return map;
-	}
-	
-	@RequestMapping("/delete_judge")
-	@ResponseBody
-	public Map<String,Object> deviceCheckDeleteJudge() throws Exception{
-		Subject subject = SecurityUtils.getSubject();
-		ActiveUser activeUser = (ActiveUser) subject.getPrincipal();
-		Map<String,Object> map = new HashMap<String,Object>();
-		if(!activeUser.getUserStatus().equals("1")){
-			map.put("msg", "您的账户已被锁定，请切换账户登录！");
-		}else if(!activeUser.getRoleStatus().equals("1")){
-			map.put("msg", "当前角色已被锁定，请切换账户登录！");
-			if(!subject.isPermitted("deviceCheck:delete")){
-				map.put("msg", "您没有权限，请切换用户登录！");
-			}
-		}
-		return map;
 	}
 	
 	/*
@@ -107,7 +48,8 @@ public class DeviceCheckController {
 	 */
 	@RequestMapping(value="/insert", method=RequestMethod.POST)
 	@ResponseBody
-	private CustomResult insert(@Valid DeviceCheck deviceCheck, BindingResult bindingResult) throws Exception {
+	private CustomResult insert(@Valid DeviceCheck deviceCheck, BindingResult bindingResult) 
+			throws Exception {
 		CustomResult result;
 		if(bindingResult.hasErrors()){
 			FieldError fieldError = bindingResult.getFieldError();
@@ -123,7 +65,8 @@ public class DeviceCheckController {
 	
 	@RequestMapping(value="/update")
 	@ResponseBody
-	private CustomResult update(@Valid DeviceCheck deviceCheck, BindingResult bindingResult) throws Exception {
+	private CustomResult update(@Valid DeviceCheck deviceCheck, BindingResult bindingResult) 
+			throws Exception {
 		if(bindingResult.hasErrors()){
 			FieldError fieldError = bindingResult.getFieldError();
 			return CustomResult.build(100, fieldError.getDefaultMessage());
@@ -133,7 +76,8 @@ public class DeviceCheckController {
 	
 	@RequestMapping(value="/update_note")
 	@ResponseBody
-	private CustomResult updateNote(@Valid DeviceCheck deviceCheck, BindingResult bindingResult) throws Exception {
+	private CustomResult updateNote(@Valid DeviceCheck deviceCheck, BindingResult bindingResult) 
+			throws Exception {
 		if(bindingResult.hasErrors()){
 			FieldError fieldError = bindingResult.getFieldError();
 			return CustomResult.build(100, fieldError.getDefaultMessage());
@@ -151,18 +95,20 @@ public class DeviceCheckController {
 	//搜索
 	@RequestMapping("/search_deviceCheck_by_deviceCheckId")
 	@ResponseBody
-	public EUDataGridResult searchDeviceCheckByDeviceCheckId(Integer page, Integer rows, String searchValue) 
-			throws Exception{
-		EUDataGridResult result = deviceCheckService.searchDeviceCheckByDeviceCheckId(page, rows, searchValue);
+	public EUDataGridResult searchDeviceCheckByDeviceCheckId(Integer page, Integer rows, 
+			String searchValue) throws Exception{
+		EUDataGridResult result = deviceCheckService
+				.searchDeviceCheckByDeviceCheckId(page, rows, searchValue);
 		return result;
 	}
 	
 	//搜索
 	@RequestMapping("/search_deviceCheck_by_deviceName")
 	@ResponseBody
-	public EUDataGridResult searchDeviceCheckByDeviceName(Integer page, Integer rows, String searchValue) 
-			throws Exception{
-		EUDataGridResult result = deviceCheckService.searchDeviceCheckByDeviceName(page, rows, searchValue);
+	public EUDataGridResult searchDeviceCheckByDeviceName(Integer page, Integer rows
+			, String searchValue) throws Exception{
+		EUDataGridResult result = deviceCheckService
+				.searchDeviceCheckByDeviceName(page, rows, searchValue);
 		return result;
 	}
 }
