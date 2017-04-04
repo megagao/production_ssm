271f4beb6a5849ebc485f55beb0f4123b98f6475
megagao <megagao@126.com>
2017-04-04 13:47:27 +0800

    修改了角色bean

--- src/main/java/org/hqu/production_ms/domain/authority/SysRole.java
+++ src/main/java/org/hqu/production_ms/domain/authority/SysRole.java
@@ -1,16 +1,18 @@
 package org.hqu.production_ms.domain.authority;
 
 public class SysRole {
-    private String roleId;
+	private String roleId;
 
     private String roleName;
 
     private String available;
+    
+    private String permission;
 
 	public String getRoleId() {
 		return roleId;
 	}
-
+  	
 	public void setRoleId(String roleId) {
 		this.roleId = roleId;
 	}
@@ -31,4 +33,11 @@ public class SysRole {
 		this.available = available;
 	}
 
+	public String getPermission() {
+		return permission;
+	}
+
+	public void setPermission(String permission) {
+		this.permission = permission;
+	}
 }
